c5ab735bc11250f5f71966176ced8cfff235e948
Milo Knowles <mknowles@mit.edu>
2017-01-29 12:28:08 -0500

    Set up query to play against the current learned strategy

--- query_cfr.py
+++ query_cfr.py
@@ -1,11 +1,11 @@
 import time
 from cfr import *
-from random import random
+from random import random, choice
 import json
 
 
 # load the cumulative strategy into memory
-with open('cumulativeStrategy.json') as csFile:   
+with open('./jsons/currentStrategy/cumulativeStrategy.json') as csFile:   
     CUMULATIVE_STRATEGY = json.load(csFile)
     print "Loaded in cumulative strategy"
 
@@ -33,6 +33,27 @@ def getStrategy(I):
 		return None
 
 
+def chooseAction(action_dict):
+    """
+    Chooses an action from a dict of action:prob pairs
+    Tested with a million choices, is very close in practice to the profile given.
+    """
+    action_dict_sum = sum(action_dict.itervalues())
+    assert (action_dict_sum < 1.03 and action_dict_sum > 0.97), "Error: Strategy profile probabilities do not add up to 1."
+
+    random_float = random()
+    cutoff = 0
+    for a in action_dict.keys():
+        cutoff += action_dict[a]
+        if random_float <= cutoff:
+            return a
+
+def chooseActionRandom(legal_actions):
+	"""
+	Chooses at random from a list of legal_actions
+	"""
+	return choice(legal_actions)
+
 def testHistoryAgainstCurrentStrategy():
     """
     Try playing out a game against the current strategy from training.
@@ -42,26 +63,49 @@ def testHistoryAgainstCurrentStrategy():
 
     while(h.NodeType != 2): # while not terminal, simulate
         if h.NodeType == 0:
-            h.printAttr()
+            #h.printAttr()
             print "-----SIMULATING CHANCE------"
             h = h.simulateChance()
-        elif h.NodeType == 1:
-            h.printAttr()
-            actions = h.getLegalActions()
-            print "Legal Actions:", actions
 
-            givenAction=False
-            while(not givenAction):
-                action = raw_input("Choose action: ")
-                if action in actions:
-                    givenAction=True
-                else:
-                    print "Action not allowed. Try again."
-            print "-----SIMULATING ACTION------"
-            h = h.simulateAction(action)
-            print convertHtoI(h, 0)
+        elif h.NodeType == 1:
+
+        	h.printAttr()
+        	actions = h.getLegalActions()
+        	print "Legal Actions:", actions
+
+        	# if it's an opponent move
+        	if h.ActivePlayer == 1:
+        		I_opp = convertHtoI(h, 1)
+        		s = getStrategy(I_opp)
+        		print "Opponent Strategy:", s
+        		opp_action = chooseActionRandom(actions) if s==None else chooseAction(s)
+        		
+        		h.simulateAction(opp_action)
+
+        	else: # our move, so ask for an action
+
+	            givenAction=False
+
+	            while(not givenAction):
+	                action = raw_input("Choose action: ")
+	                if action in actions:
+	                    givenAction=True
+	                else:
+	                    print "Action not allowed. Try again."
+
+	            print "-----SIMULATING ACTION------"
+	            h = h.simulateAction(action)
+
         else:
             print "ERROR, not recognized nodetype"
 
     print "------TERMINAL NODE REACHED------"
     h.printAttr()
+    p1_util, p2_util = h.getTerminalUtilities()
+    print "P1_Util:", p1_util, "P2_Util:", p2_util
+
+
+testHistoryAgainstCurrentStrategy()
+
+
+
