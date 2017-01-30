b2a95bf1a8040066c17fed5cd2089143f91b9787
Milo Knowles <mknowles@mit.edu>
2017-01-29 20:37:08 -0500

    Added function to handle discards

--- Pineapple.py
+++ Pineapple.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env 
 
+import argparse
+import socket
+import sys
 import time
 from random import random, choice
+from pokereval.card import Card
 from query_cfr import chooseAction, chooseActionRandom, getStrategy
 from cfr import getHandStrength, determineBestDiscardFast, convertHtoI, LightweightHistory
 import json
@@ -15,11 +19,53 @@ from ParsePackets import *
 # when we get to a discard, use the History to determine which one?
 
 
-global NEWGAME_PACKET, NEWHAND_PACKET, GETACTION_PACKET
+global NEWGAME_PACKET, NEWHAND_PACKET, GETACTION_PACKET, HISTORY,
+CURRENT_ROUND = 0 # chance round
+
+# (self, history, node_type, current_street, current_round, button_player, \
+# active_player, pot, p1_inpot, p2_inpot, bank_1, bank_2, p1_hand, p2_hand, board):
+HISTORY = LightweightHistory([], 0, 0, 0, 0, 0, 0, 0, 0, 200, 200, [], [], [])
 
 
 def mapBetToAbstraction(bet_amt, bet_options):
 	raise NotImplementedError
+
+
+def resetHistory():
+    """
+    Resets the global HISTORY to defaults.
+    """
+    global HISTORY
+    HISTORY.P1_inPot, HISTORY.p2_inPot, HISTORY.Pot = 0, 0, 0 # reset the in-pot and pot for this street
+    HISTORY.ButtonPlayer = 0 if NEWHAND_PACKET.button==True else 1
+    HISTORY.P1_Bankroll, HISTORY.P2_Bankroll = 200, 200 # reset both bankrolls to 200
+    HISTORY.Street, HISTORY.CurrentRound, HISTORY.NodeType = 0,0,0 # preflop
+    HISTORY.P1_Hand, HISTORY.P2_Hand, HISTORY.Board = [], [], []
+
+
+def handleDiscard(hand, board):
+    """
+    Decides whether to discard, and which card.
+    Returns either "CHECK" or "DISCARD:*card*", which is the action to be sent.
+    """
+    # first, do a sanity check
+    global GETACTION_PACKET
+    assert ("DISCARD:%s" % hand[0]) in GETACTION_PACKET.legalActions
+
+    shouldSwap, swapIndex, swapEV, originalEV = determineBestDiscardFast(hand, board)
+
+    if shouldSwap:
+        return "DISCARD:%s\n" % hand[swapIndex]
+    else:
+        return "CHECK\n"
+
+
+def extractOppBetting():
+    """
+    Reads through lastActions in GETACTION_PACKET and updates global information if an opponent betting action was made.
+    """
+    global GETACTION_PACKET
+    for a in GETACTION_PACKET.lastActions:
 
 
 class Player:
@@ -38,17 +84,39 @@ class Player:
 
             print data # print the latest packet
 
+            # determine what type of packet this is
             word = data.split()[0]
 
             if word == "GETACTION":
+                # store the current action information globally
+                global GETACTION_PACKET, HISTORY
                 GETACTION_PACKET = GETACTION(data)
 
+                if "DEAL:FLOP" in GETACTION_PACKET.lastActions:
+                	HISTORY.P1_inPot, P2_inPot = 0, 0 # reset the in pot for this street
+
+            	elif "DEAL:TURN" in GETACTION_PACKET.lastActions:
+            		HISTORY.P1_inPot, P2_inPot = 0, 0 # reset the in pot for this street
+
+            	elif "DEAL:RIVER" in GETACTION_PACKET.lastActions
+            		HISTORY.P1_inPot, P2_inPot = 0, 0 # reset the in pot for this street
+
+
             elif word=="NEWGAME":
+
+                # store game information globally
+            	global NEWGAME_PACKET
                 NEWGAME_PACKET = NEWGAME(data)
 
+
             elif word=="NEWHAND":
+
+                # update the global NEWHAND packet
+            	global NEWHAND_PACKET
                 NEWHAND_PACKET = NEWHAND(data)
 
+                # reset the global HISTORY to defaults for the start of a hand
+                resetHistory()
 
             elif word == "REQUESTKEYVALUES":
                 # At the end, the engine will allow your bot save key/value pairs.
@@ -57,7 +125,10 @@ class Player:
         # Clean up the socket.
         s.close()
 
+
+
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(description='A Pokerbot.', add_help=False, prog='pokerbot')
     parser.add_argument('-h', dest='host', type=str, default='localhost', help='Host to connect to, defaults to localhost')
     parser.add_argument('port', metavar='PORT', type=int, help='Port on host to connect to')
